6353b08ce9ac086a28a69f5fb7688a6b224a580b
Krishna Kumar <110533084+krishnakumar0821@users.noreply.github.com>
2023-06-01 23:12:47 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -9,9 +9,11 @@ function updateClock(){
     let s = new Date().getSeconds()
     let ampm = "AM"
 
-    if(h < 12){
-        h = h - 12
-        ampm = "PM"
+      if (h >= 12) {
+        ampm = "PM";
+        if (h > 12) {
+            h = h - 12;
+        }
     }
 
     h = h < 10 ? "0" + h : h;
